a88b088e6575683e83b986462cfc9ca24c77e29d
Redoxahmii <fullmetal2334@gmail.com>
2024-10-17 04:29:47 +0500

    fix(cloudinary): added cloudinary in util

--- src/utils/uploadCloudinary.js
+++ src/utils/uploadCloudinary.js
@@ -1,3 +1,12 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
 const uploadToCloudinary = (fileBuffer) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
